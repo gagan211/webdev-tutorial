4f0960251c2c9f27074fedfc6706e939f1dfa9a4
GAGAN M <gagankhivesara1@gmail.com>
2024-02-04 18:33:11 +0530

    started javascript

---
+++ javascript/script.js
@@ -0,0 +1,16 @@
+alert("hello world");
+
+// console.log("wassup")
+// console.log("code is running")
+// console.log("code is running ow")
+
+// var a=prompt("enter your numeber")
+// console.log("your number is "+a)
+var istrue= confirm("are you sure to leave this page")
+if(istrue)
+{
+    console.log("computer is bblasting")
+}
+else{
+    console.log("npot eaving the page")
+}
